a48331cd1f1bea916e4e81622294c2934c9e098f
LucasRoberts <LucasJRoberts36@gmail.com>
2021-10-05 16:56:54 -0400

    - Added catches to catch for: faulty job title from user input and requesting too many jobs - Added comments

--- main.py
+++ main.py
@@ -3,8 +3,6 @@ Author: Lucas Roberts
 Date: 9/30/21
 Description: I will be scrapping indeed for job listings, grabbing certain items
 """
-import sys
-
 import pandas
 import selenium.common.exceptions
 from selenium import webdriver
@@ -12,81 +10,103 @@ from selenium.webdriver.common.keys import Keys
 import pandas as pd
 import time
 
-# Setting up Chromedriver
-PATH = "chromedriver.exe"
-driver = webdriver.Chrome(PATH)
 
-# Grabbing Job Title
-user_input = input("Enter the job title: ").lower()
-user_input.replace(" ", "%20")
-URL = f"https://www.glassdoor.com/Job/jobs.htm?sc.keyword={user_input}"
-driver.get(URL)
-# Close closes a single table quit() will completely close all
-jobs = int(input("Enter how many jobs would you like to find: "))
-counter = 1
-running = True
-company_name = []
-job_title = []
-location = []
-pay = []
-job_age = []
+def main():
+    # Setting up Chromedriver
+    PATH = "chromedriver.exe"
+    driver = webdriver.Chrome(PATH)
 
-# Get rid of the pop-up
-temp_elem = driver.find_element_by_class_name("react-job-listing")
-temp_elem.click()
-close_popup = driver.find_element_by_class_name("modal_closeIcon-svg")
-close_popup.click()
+    # Grabbing Job Title
+    user_input = input("Enter the job title: ").lower()
+    user_input.replace(" ", "%20")
+    URL = f"https://www.glassdoor.com/Job/jobs.htm?sc.keyword={user_input}"
+    driver.get(URL)
 
+    # Close closes a single table quit() will completely close all
+    jobs = int(input("Enter how many jobs would you like to find: "))
+    counter = 1
+    running = True
+    company_name = []
+    job_title = []
+    location = []
+    pay = []
+    job_age = []
 
-while running:
-    for job_number in range(1, jobs+1, 1):
-        print(job_number)
+    # Check how many jobs are on the page after user inputted job title
+    time.sleep(5)
+    actual_jobs = driver.find_element_by_xpath("//div[@data-test='MainColSummary']/p[@data-test='jobsCount']").get_attribute("textContent")
+    actual_jobs = int(actual_jobs.strip(" jobs"))
 
-        # Grabs the company name
-        try:
-            company_name.append(driver.find_element_by_xpath(f"//li[{counter}]/div[2]/div[1]/a[1]/span").text)
-        except selenium.common.exceptions.NoSuchElementException:
-            company_name.append(-1)
+    # If there are no jobs, exits the program
+    if actual_jobs == 0:
+        print("You entered an incorrect job title or one with 0 job postings.\n Try searching another job title.")
+        driver.close()
+        return -1
+    # If the jobs requested are more than the amount listed, lowers the amount requested to amount listed.
+    elif jobs > actual_jobs:
+        print("The amount of jobs you want are less than how many there are.\n Setting requested jobs to the amount"
+              "listed on the site.")
+        jobs = actual_jobs
 
-        # Grabs the title of the job
-        try:
-            job_title.append(driver.find_element_by_xpath(f"//li[{counter}]/div[2]/a[1]/span").text)
-        except selenium.common.exceptions.NoSuchElementException:
-            job_title.append(-1)
+    # Get rid of the pop-up
+    temp_elem = driver.find_element_by_class_name("react-job-listing")
+    temp_elem.click()
+    close_popup = driver.find_element_by_class_name("modal_closeIcon-svg")
+    close_popup.click()
 
-        # Grabs the location of the job
-        try:
-            location.append(driver.find_element_by_xpath(f"//li[{counter}]/div[2]/div[2]/span").text)
-        except selenium.common.exceptions.NoSuchElementException:
-            location.append(-1)
+    while running:
+        for job_number in range(1, jobs+1, 1):
+            print(job_number)
 
-        # Grabs how much the salary is
-        try:
-            pay.append(driver.find_element_by_xpath(f"//li[{counter}]/div[2]/div[3]/div[1]/span").text)
-        except selenium.common.exceptions.NoSuchElementException:
-            pay.append(-1)
+            # Grabs the company name
+            try:
+                company_name.append(driver.find_element_by_xpath(f"//li[{counter}]/div[2]/div[1]/a[1]/span").text)
+            except selenium.common.exceptions.NoSuchElementException:
+                company_name.append(-1)
 
-        # Grabs how long the job has been up
-        try:
-            job_age.append(driver.find_element_by_xpath(f"//li[{counter}]/div[2]/div[3]/div[2]/div[2]").text)
-        except selenium.common.exceptions.NoSuchElementException:
-            job_age.append(-1)
+            # Grabs the title of the job
+            try:
+                job_title.append(driver.find_element_by_xpath(f"//li[{counter}]/div[2]/a[1]/span").text)
+            except selenium.common.exceptions.NoSuchElementException:
+                job_title.append(-1)
 
-        # Added a sleep due to pages not loading correctly
-        # most likely will move this to after the next page button click and make it longer
-        time.sleep(1)
+            # Grabs the location of the job
+            try:
+                location.append(driver.find_element_by_xpath(f"//li[{counter}]/div[2]/div[2]/span").text)
+            except selenium.common.exceptions.NoSuchElementException:
+                location.append(-1)
 
-        counter += 1
-        if counter == jobs:
-            running = False
-        elif counter == 30:
-            # Clicks the next page button
-            driver.find_element_by_xpath("//a[@data-test='pagination-next']").click()
-            counter = 0
-            jobs -= 30
+            # Grabs how much the salary is
+            try:
+                pay.append(driver.find_element_by_xpath(f"//li[{counter}]/div[2]/div[3]/div[1]/span").text)
+            except selenium.common.exceptions.NoSuchElementException:
+                pay.append(-1)
+
+            # Grabs how long the job has been up
+            try:
+                job_age.append(driver.find_element_by_xpath(f"//li[{counter}]/div[2]/div[3]/div[2]/div[2]").text)
+            except selenium.common.exceptions.NoSuchElementException:
+                job_age.append(-1)
+
+            # Added a sleep due to pages not loading correctly
+            # most likely will move this to after the next page button click and make it longer
+
+            if counter == jobs:
+                running = False
+            elif counter == 30:
+                # Clicks the next page button
+                driver.find_element_by_xpath("//a[@data-test='pagination-next']").click()
+                counter = 0
+                jobs -= 30
+                time.sleep(5)
+            counter += 1
     print(company_name)
     print(job_title)
     print(location)
     print(pay)
     print(job_age)
-driver.close()
+    driver.close()
+
+
+if __name__ == "__main__":
+    main()
