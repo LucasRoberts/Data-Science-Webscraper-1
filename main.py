6dde7ca517fb6f9614ae60c4e61a70f5ff88898f
LucasRoberts <LucasJRoberts36@gmail.com>
2021-10-04 22:00:41 -0400

    - Retrieves Company name, Job Title, Location, Salary, and job age

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ Author: Lucas Roberts
 Date: 9/30/21
 Description: I will be scrapping indeed for job listings, grabbing certain items
 """
+import sys
+
 import pandas
 import selenium.common.exceptions
 from selenium import webdriver
@@ -19,11 +21,57 @@ user_input.replace(" ", "%20")
 URL = f"https://www.glassdoor.com/Job/jobs.htm?sc.keyword={user_input}"
 driver.get(URL)
 # Close closes a single table quit() will completely close all
+# jobs = input("How many jobs would you like to grab: ")
+jobs = 20
+counter = 1
+running = True
+company_name = []
+job_title = []
+location = []
+pay = []
+job_age = []
 
-try:
-    # company_name = driver.find_elements_by_css_selector(".job-search-key-l2wjgv e1n63ojh0 jobLink span")
-    company_name = driver.find_element_by_xpath("//li[@class=react-job-listing css-bkasv9 eigr9kq0]/div[1]/div/a/span")
-except selenium.common.exceptions.InvalidSelectorException:
-    company_name = -1
-print(company_name)
-jobs = []
+# Get rid of the pop-up
+temp_elem = driver.find_element_by_class_name("react-job-listing")
+temp_elem.click()
+close_popup = driver.find_element_by_class_name("modal_closeIcon-svg")
+close_popup.click()
+
+
+while running:
+    for job_number in range(1, jobs+1, 1):
+        print(job_number)
+        try:
+            company_name.append(driver.find_element_by_xpath(f"//li[{job_number}]/div[2]/div[1]/a[1]/span").text)
+        except selenium.common.exceptions.NoSuchElementException:
+            company_name.append(-1)
+
+        try:
+            job_title.append(driver.find_element_by_xpath(f"//li[{job_number}]/div[2]/a[1]/span").text)
+        except selenium.common.exceptions.NoSuchElementException:
+            job_title.append(-1)
+
+        try:
+            location.append(driver.find_element_by_xpath(f"//li[{job_number}]/div[2]/div[2]/span").text)
+        except selenium.common.exceptions.NoSuchElementException:
+            location.append(-1)
+
+        try:
+            pay.append(driver.find_element_by_xpath(f"//li[{job_number}]/div[2]/div[3]/div[1]/span").text)
+        except selenium.common.exceptions.NoSuchElementException:
+            pay.append(-1)
+
+        try:
+            job_age.append(driver.find_element_by_xpath(f"//li[{job_number}]/div[2]/div[3]/div[2]/div[2]").text)
+        except selenium.common.exceptions.NoSuchElementException:
+            job_age.append(-1)
+
+        counter += 1
+        if counter == jobs:
+            running = False
+    print(company_name)
+    print(job_title)
+    print(location)
+    print(pay)
+    print(job_age)
+sys.exit()
